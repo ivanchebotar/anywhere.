6a1c4b36efb86a7015c40dbe2c5eb69725d793aa
1 <a1@MacBook-Pro-Ivan-Chebotar.local>
2022-09-06 17:40:18 +0300

    new files

--- markup/src/js/components/mobileNav.js
+++
@@ -1,9 +0,0 @@
-import 'Plugins/mobileNavPlugin';
-
-// open-close init
-export function initMobileNav() {
-  jQuery('body').mobileNav({
-		menuActiveClass: 'menu-active',
-		menuOpener: '.header-menu__opener'
-	});
-}
